a263167852e31c2f4e4a845ccc8ac7b14bced652
Jovceo <lhqlvp@qq.com>
2026-01-13 11:22:26 +0800

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/header";
+import Script from "next/script";
 import { Footer } from "@/components/footer";
 import Breadcrumbs from "@/components/breadcrumbs";
 import { Geist } from "next/font/google";
@@ -73,6 +74,20 @@ export default async function RootLayout({
           </div>
           <Toaster />
         </ThemeProvider>
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+        </Script>
       </body>
     </html>
   );
